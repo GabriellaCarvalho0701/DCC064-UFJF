2dc51e8016db70da5d5e459e2c2dec77a8b8e42e
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2023-12-01 14:51:34 -0300

    fix: List sensor

--- routes/Sensor/index.ts
+++ routes/Sensor/index.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
 import { Sensor } from "../../models/Sensor";
 
 const router = express.Router();
 
-router.get("/sensor", [], async (req: Request, res: Response) => {
-  const sensor = await Sensor.find({});
-  return res.status(200).send(sensor);
+router.get("/sensor/:id_collector", [], async (req: Request, res: Response) => {
+  const { id_collector } = req.params;
+  console.log(id_collector);
+
+  try {
+    const sensors = await Sensor.find({ id_coletor: id_collector });
+    return res.status(200).json({
+      sensors,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 });
 
 router.get(
